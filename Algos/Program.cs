14322f7250f6cd9ab8da438d2bf38fe0133bc626
ionuttamas <tamash_ionut@hotmail.com>
2016-05-10 00:38:37 +0300

    Initial commit

---
+++ Algos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algos {
+    class Program {
+        static void Main(string[] args) {
+        }
+
+        public float[] Sort(float[] list) {
+            for (int i = 0; i < list.Length-1; i++)
+            {
+                for (int j = i+1; j < list.Length; j++) {
+                    if (list[i] > list[i + 1])
+                    {
+                        float temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i+1] = temp;
+                    }
+                }
+            } 
+
+            return list;
+        }
+    }
+}
